ea9ec7111eaaf35d47d96994d3f8da8d82ad82d6
blacko012 <mrsmith.hblk@gmail.com>
2023-08-10 18:15:23 -0700

    c-low_level_programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description A C program thats prints with put function
+*
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+        puts("\"Progamming is like building multilingual puzzle"):
+        return (0)
+}
+
