8679e0245708903b11c420405aa126bdb0ca91aa
blacko012 <mrsmith.hblk@gmail.com>
2023-08-10 18:45:01 -0700

    c-low_level_programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
-*main - Entry point
-*
-*Retrun: Always 0 (success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
-        puts("Progamming is like building multilingual puzzle\n");
-        return (0);
+    puts("Programming is like building a multilingual puzzle\n");
+    return (0);
 }
