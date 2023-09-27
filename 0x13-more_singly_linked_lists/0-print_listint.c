fb46b07edeb7a421a4df337d0e09647bfa8d8165
blacko012 <mrsmith.hblk@gmail.com>
2023-09-27 06:33:56 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
+ * print_listint - prints all the elements
+ * of a linked list
  * @h: head of the list
  *
  * Return: the number of nodes
- */
- size_t listint_len(const listint_t *h)
+*/
+size_t listint_len(const listint_t *h)
 {
- size_t count = 0;
+size_t number = 0;
 
- while (h)
- {
- num++;
- h = h->next;
- }
+while (h)
+{
+number++;
+h = h->next;
+}
 
- return (count);
+return (number);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,13 +10,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
- size_t number = 0;
+size_t number = 0;
 
- while (h)
- {
- number++;
- h = h->next;
- }
+while (h)
+{
+number++;
+h = h->next;
+}
 
- return (number);
+return (number);
 }
