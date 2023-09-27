c7048dec1bc924bc4c831836dfcc9808739e482b
blacko012 <mrsmith.hblk@gmail.com>
2023-09-27 16:09:20 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,12 @@
 */
 size_t listint_len(const listint_t *h)
 {
-size_t n = 0;
+	size_t i = 0;
 
-while (h)
-{
-n++;
-h = h->next;
-}
-
-return (n);
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (n);
 }
