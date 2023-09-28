5a920468072bd206a696db910a5019bf778bec9d
blacko012 <mrsmith.hblk@gmail.com>
2023-09-27 18:08:47 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -7,45 +7,38 @@
  * @n: The value to be added to the new node.
  * Return: The address of the new node, or NULL if it failed.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current;
-	unsigned int i;
+unsigned int i;
+listint_t *new;
+listint_t *temp = *head;
 
-	if (head == NULL)
-		return (NULL);
+new = malloc(sizeof(listint_t));
+if (!new || !head)
+return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
+new->n = n;
+new->next = NULL;
 
-	new_node->n = n;
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
 
-	if (idx == 0)
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
+for (i = 0; temp && i < idx; i++)
+{
+if (i == idx - 1)
+{
+new->next = temp->next;
+temp->next = new;
+return (new);
+}
+else
+temp = temp->next;
+}
 
-	current = *head;
-	for (i = 0; i < idx - 1; i++)
-	{
-		if (current == NULL)
-		{
-			free(new_node);
-			return (NULL);
-		}
-		current = current->next;
-	}
-
-	if (current == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
+return (NULL);
 }
