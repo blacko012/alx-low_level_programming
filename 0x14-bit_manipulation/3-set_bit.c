4cc300522f9fb8bceb43e67a054cd7050f2ec4c8
blacko012 <mrsmith.hblk@gmail.com>
2023-09-29 12:40:43 -0700

    bit_manipulation

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,8 +10,8 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index > 63)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 
-	return ((n >> index) & 1);
+	return (!!(*n |= 1L << index));
 }
