c4951ae8afe52441fdfc25bc6ea719b2f041f31a
blacko012 <mrsmith.hblk@gmail.com>
2023-09-29 15:01:04 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,33 +1,41 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - Finds the start of a loop in a linked list.
- * @head: A pointer to the head of the linked list.
- * Return: The address of the node where the loop starts, or NULL if no loop.
+ * find_listint_loop - Finds the loop contained in,
+ * a listint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: If there is no loop - NULL.
+ *         Otherwise - the address of the node where the loop starts.
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-listint_t *red = head;
-listint_t *black = head;
+	listint_t *nodeint, *buck;
 
-if (!head)
-	return (NULL);
+	if (head == NULL || head->next == NULL)
+		return (NULL);
 
-while (red && black && black->next)
-{
-black = black->next->next;
-red = red->next;
-if (black == red)
-{
-red = head;
-while (red != black)
-{
-red = red->next;
-black = black->next;
-}
-	return (black);
-}
-}
+	nodeint = head->next;
+	buck = (head->next)->next;
+
+	while (buck)
+	{
+		if (nodeint == buck)
+		{
+			nodeint = head;
+
+			while (nodeint != buck)
+			{
+				nodeint = nodeint->next;
+				buck = buck->next;
+			}
+
+			return (nodeint);
+		}
+
+		nodeint = nodeint->next;
+		buck = (buck->next)->next;
+	}
 
 	return (NULL);
 }
