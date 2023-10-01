0127d380913071d029640fd2d53340a67604decf
blacko012 <mrsmith.hblk@gmail.com>
2023-10-01 14:20:22 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t nnodes = 0;
 
 	while (h)
 	{
-		nodes++;
+		nnodes++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
 
-	return (nodes);
+	return (nnodes);
 }
