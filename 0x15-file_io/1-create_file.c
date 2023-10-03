0eae122ea126c1c7b6f2b6edb2c6f3085871ff72
blacko012 <mrsmith.hblk@gmail.com>
2023-10-03 05:30:21 -0700

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,16 +14,16 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
-	if (op == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
 		for (letr = 0; text_content[letr];)
 			letr++;
 	}
+
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (op == -1)
+		return (-1);
 
 	wr = write(op, text_content, letr);
 
