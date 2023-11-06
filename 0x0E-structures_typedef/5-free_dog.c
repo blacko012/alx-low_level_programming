ec8685888e1055c355fd3b5e9aa383b9e1980239
blacko012 <mrsmith.hblk@gmail.com>
2023-11-06 14:44:53 -0800

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+*free_dog - Frees memory allocated for a struct dog
+*@d: dog to be freed
+*
+*/
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+return;
+
+free(d->name);
+free(d->owner);
+free(d);
+}
---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+this is my readme file aout :0x0E-structures_typedef
